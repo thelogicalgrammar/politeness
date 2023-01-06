47479e813f996c6d23511b99b8093e7f1b2a6bf7
Fausto Carcassi <fausto.carcassi@gmail.com>
2023-01-06 15:14:20 +0100

    Increase server job time

--- server_jobs/job_mymodel.sh
+++ server_jobs/job_mymodel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=single
 #SBATCH --ntasks=4
-#SBATCH --time=00:10:00
+#SBATCH --time=10:00:00
 
 module load devel/miniconda/3
 source $MINICONDA_HOME/etc/profile.d/conda.sh
